cf22b33da00afafd39d43348c295e8e1d73095ca
Allisson Azevedo <allisson@gmail.com>
2023-04-03 20:49:48 -0300

    feat: add UpdateWithOptionsQuery and DeleteWithOptionsQuery functions (#9)

--- query.go
+++ query.go
@@ -15,7 +15,7 @@ func parseIn(value string) []interface{} {
 	return result
 }
 
-func parseFilter(sb *sqlbuilder.SelectBuilder, key string, value interface{}) {
+func parseSelectFilter(sb *sqlbuilder.SelectBuilder, key string, value interface{}) {
 	if strings.Contains(key, ".") {
 		split := strings.Split(key, ".")
 		parsedKey := split[0]
@@ -65,13 +65,113 @@ func parseFilter(sb *sqlbuilder.SelectBuilder, key string, value interface{}) {
 	}
 }
 
+func parseUpdateFilter(ub *sqlbuilder.UpdateBuilder, key string, value interface{}) {
+	if strings.Contains(key, ".") {
+		split := strings.Split(key, ".")
+		parsedKey := split[0]
+		compare := split[1]
+		switch compare {
+		case "in":
+			valueStr, ok := value.(string)
+			if ok {
+				values := parseIn(valueStr)
+				ub.Where(ub.In(parsedKey, values...))
+			}
+		case "notin":
+			valueStr, ok := value.(string)
+			if ok {
+				values := parseIn(valueStr)
+				ub.Where(ub.NotIn(parsedKey, values...))
+			}
+		case "not":
+			ub.Where(ub.NotEqual(parsedKey, value))
+		case "gt":
+			ub.Where(ub.GreaterThan(parsedKey, value))
+		case "gte":
+			ub.Where(ub.GreaterEqualThan(parsedKey, value))
+		case "lt":
+			ub.Where(ub.LessThan(parsedKey, value))
+		case "lte":
+			ub.Where(ub.LessEqualThan(parsedKey, value))
+		case "like":
+			ub.Where(ub.Like(parsedKey, value))
+		case "null":
+			valueBool, ok := value.(bool)
+			if ok {
+				if valueBool {
+					ub.Where(ub.IsNull(key))
+				} else {
+					ub.Where(ub.IsNotNull(key))
+				}
+			}
+		}
+	} else {
+		switch value.(type) {
+		case nil:
+			ub.Where(ub.IsNull(key))
+		default:
+			ub.Where(ub.Equal(key, value))
+		}
+	}
+}
+
+func parseDeleteFilter(db *sqlbuilder.DeleteBuilder, key string, value interface{}) {
+	if strings.Contains(key, ".") {
+		split := strings.Split(key, ".")
+		parsedKey := split[0]
+		compare := split[1]
+		switch compare {
+		case "in":
+			valueStr, ok := value.(string)
+			if ok {
+				values := parseIn(valueStr)
+				db.Where(db.In(parsedKey, values...))
+			}
+		case "notin":
+			valueStr, ok := value.(string)
+			if ok {
+				values := parseIn(valueStr)
+				db.Where(db.NotIn(parsedKey, values...))
+			}
+		case "not":
+			db.Where(db.NotEqual(parsedKey, value))
+		case "gt":
+			db.Where(db.GreaterThan(parsedKey, value))
+		case "gte":
+			db.Where(db.GreaterEqualThan(parsedKey, value))
+		case "lt":
+			db.Where(db.LessThan(parsedKey, value))
+		case "lte":
+			db.Where(db.LessEqualThan(parsedKey, value))
+		case "like":
+			db.Where(db.Like(parsedKey, value))
+		case "null":
+			valueBool, ok := value.(bool)
+			if ok {
+				if valueBool {
+					db.Where(db.IsNull(key))
+				} else {
+					db.Where(db.IsNotNull(key))
+				}
+			}
+		}
+	} else {
+		switch value.(type) {
+		case nil:
+			db.Where(db.IsNull(key))
+		default:
+			db.Where(db.Equal(key, value))
+		}
+	}
+}
+
 // FindQuery returns compiled SELECT string and args.
 func FindQuery(tableName string, options *FindOptions) (string, []interface{}) {
 	sb := sqlbuilder.NewSelectBuilder()
 	sb.SetFlavor(sqlbuilder.Flavor(options.Flavor))
 	sb.Select(options.Fields...).From(tableName)
 	for key, value := range options.Filters {
-		parseFilter(sb, key, value)
+		parseSelectFilter(sb, key, value)
 	}
 	return sb.Build()
 }
@@ -82,7 +182,7 @@ func FindAllQuery(tableName string, options *FindAllOptions) (string, []interfac
 	sb.SetFlavor(sqlbuilder.Flavor(options.Flavor))
 	sb.Select(options.Fields...).From(tableName).Limit(options.Limit).Offset(options.Offset)
 	for key, value := range options.Filters {
-		parseFilter(sb, key, value)
+		parseSelectFilter(sb, key, value)
 	}
 	if options.OrderBy != "" {
 		sb.OrderBy(options.OrderBy)
@@ -117,5 +217,30 @@ func DeleteQuery(flavor Flavor, tableName string, id interface{}) (string, []int
 	db.SetFlavor(sqlbuilder.Flavor(flavor))
 	db.DeleteFrom(tableName)
 	db.Where(db.Equal("id", id))
+	return db.Build()
+}
+
+// UpdateWithOptionsQuery returns compiled UPDATE string and args from UpdateOptions.
+func UpdateWithOptionsQuery(tableName string, options *UpdateOptions) (string, []interface{}) {
+	ub := sqlbuilder.NewUpdateBuilder()
+	ub.SetFlavor(sqlbuilder.Flavor(options.Flavor))
+	ub.Update(tableName)
+	for key, value := range options.Assignments {
+		ub.Set(ub.Assign(key, value))
+	}
+	for key, value := range options.Filters {
+		parseUpdateFilter(ub, key, value)
+	}
+	return ub.Build()
+}
+
+// DeleteWithOptionsQuery returns compiled DELETE string and args from DeleteOptions.
+func DeleteWithOptionsQuery(tableName string, options *DeleteOptions) (string, []interface{}) {
+	db := sqlbuilder.NewDeleteBuilder()
+	db.SetFlavor(sqlbuilder.Flavor(options.Flavor))
+	db.DeleteFrom(tableName)
+	for key, value := range options.Filters {
+		parseDeleteFilter(db, key, value)
+	}
 	return db.Build()
 }
